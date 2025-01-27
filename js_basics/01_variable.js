a13057ea225b679ab4935d12730117d826eae116
Dinesh5143 <122430039+Dinesh5143@users.noreply.github.com>
2025-01-27 15:45:35 +0000

    learnt data types

---
+++ js_basics/01_variable.js
@@ -0,0 +1,12 @@
+
+const employeeId = 1909283;
+if (employeeId== 1909283){
+    var employeeName = "DineshReddy";
+}
+employeeName="Ritesh";
+let companyName = "comcast";
+let employeeSalary; //return undefined
+/* 
+prefer not to use 'var' because it is not defined to a scope , so its accesed outside of scope also.
+*/
+console.table([employeeId, employeeName,companyName,employeeSalary]);
---
+++ js_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let name = "Dinesh";
+let age= 22;
+let employed = true;
+let company;
+
+console.log(null);
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof name); // string
+console.log(typeof null); //object
