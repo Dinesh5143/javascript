f9a635846458209ef1840ff7480e589cd6e319c2
Dinesh5143 <122430039+Dinesh5143@users.noreply.github.com>
2025-01-28 09:59:27 +0000

    datatypes summary and comparision

---
+++ js_basics/04_comparison.js
@@ -0,0 +1,14 @@
+"use strict"
+// console.log(2>"1");
+// console.log(null==0);
+// console.log(null< 0);
+// console.log(null>=0);
+
+
+// console.log(undefined==0);
+// console.log(undefined< 0);
+// console.log(undefined>=0);
+
+// console.log(null == undefined); ->true
+
+// === checks the type of value and value
---
+++ js_basics/datatypes_summary.js
@@ -0,0 +1,31 @@
+//primitive datatypes call by value.
+// string,number,BigInt, boolean, null,undefined, symbol(to make a value unique)
+
+//non-primtive datatypes ->refernce type
+// arrays, objects, functions,
+
+// diff by call by value and call by ref
+
+// in call by value , actually we send a copy of the value, so 
+// if we change it inside a function its values doesnt get change.
+
+let studentName= "caki";
+let functionName = function changeName(Name){
+    Name="ritwik"
+}
+functionName(studentName);
+console.log(studentName);
+//call by value all primitve datatypes.
+
+let myObject1={name:"dinesh"};
+let myObject2 = myObject1;
+myObject2.name="ritesh"
+console.log(myObject1.name);
+//call by refernce all non primitive datatypes.
+
+let BigInt = 78378368778343767n;
+console.log(typeof BigInt);
+// console.log(typeof functionName); ->object function
+
+// typeof null -> object 
+// typeof all non primitive datatypes are ->object;
